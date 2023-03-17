f0e550ca743c110588a688c875d158df56527829
inviisiblejessica <eunjung@inviisible.co>
2023-03-18 07:54:19 +0900

    Feature README.md

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,6 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 // this manifest is used temporarily for development purposes
 const manifestUrl = 'https://howl-castle.github.io/PODO-TON/tonconnect-manifest.json';
 
-
-// const manifestUrl = '../podo/tonconnect-manifest.json'
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
