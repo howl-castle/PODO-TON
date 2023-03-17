950055c699e3cb7fd0ac5d13613cfc9b0a0a5b8a
inviisiblejessica <eunjung@inviisible.co>
2023-03-18 07:37:44 +0900

    feature FE

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { TonConnectButton } from '@tonconnect/ui-react';
+import { useTonConnect } from './hooks/useTonConnect';
+import { useCounterContract } from './hooks/useCounterContract';
+
+function App() {
+  const { connected } = useTonConnect();
+  const { value, address, sendIncrement } = useCounterContract();
+
+  return (
+    <div className='App'>
+      <div className='Container'>
+        <TonConnectButton />
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App
